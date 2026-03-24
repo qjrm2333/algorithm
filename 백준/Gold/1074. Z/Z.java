1ee13531508cd2ddeaa12c96a3079c462293d104
Lee Byeung Geon <102504014+qjrm2333@users.noreply.github.com>
2026-03-24 23:01:42 +0900

    [Gold V] Title: Z, Time: 124 ms, Memory: 15904 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -2,42 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
-    static long cnt = 0;
-    static int r, c;
+    static int N, R, C, cnt;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-
-        split(r, c, (int) Math.pow(2, N));
-        bw.write(cnt+"\n");
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        cnt = 0;
+        divide(R, C, N);
+        bw.write(cnt + "\n");
         bw.flush();
-        return;
     }
 
-    private static void split(int x, int y, int len){
-        if(len == 1)
+    static void divide(int r, int c, int n) throws IOException {
+        if(n == 0){
             return;
-
-        if(x < len/2 && y < len/2){
-            split(x, y, len/2);
-        } else if(x < len/2 && y >= len/2){
-            cnt += len * len / 4;
-            split(x, y-len/2, len/2);
-        } else if(x >= len/2 && y < len/2){
-            cnt += len * len / 4 * 2;
-            split(x-len/2, y, len/2);
-        } else if(x >= len/2 && y >= len/2){
-            cnt += len * len / 4 * 3;
-            split(x-len/2, y-len/2, len/2);
         }
-    } 
-    
+
+        int tmp = (int) Math.pow(2, (n-1));
+        if(r < tmp && c < tmp){
+            divide(r, c, n-1);
+        } else if(r < tmp && c >= tmp){
+            cnt += tmp * tmp;
+            divide(r, c-tmp, n-1);
+        } else if(r >= tmp && c < tmp){
+            cnt += tmp * tmp * 2;
+            divide(r-tmp, c, n-1);
+        } else {
+            cnt += tmp * tmp * 3;
+            divide(r-tmp, c-tmp, n-1);
+        }
+    }
 }
