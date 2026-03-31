9328ba3b3c6862d0062be95ec2b17052c297a0ba
Lee Byeong Geon <102504014+qjrm2333@users.noreply.github.com>
2026-03-31 10:24:24 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 132 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int N;
+    static int[] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        dp = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 0;
+        for(int i = 0; i < N; i++){
+            dp[i] = arr[i];
+            for(int j = 0; j < i; j++){
+                if(arr[i] > arr[j]){
+                    dp[i] = Math.max(dp[j]+arr[i], dp[i]);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        bw.write(max+"");
+        bw.flush();
+    }
+}
