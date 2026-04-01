0d7cbf1975e681a19da5a31c16f88966e5c7b0db
Lee Byeong Geon <102504014+qjrm2333@users.noreply.github.com>
2026-04-01 20:16:24 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 440 ms, Memory: 39692 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int N, ans = 0;
+    static int[][] map;
+    static int[][] dp;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        dp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.fill(dp[i], -1);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                ans = Math.max(ans, dfs(i, j));
+            }
+        }
+
+
+        bw.write(ans + "");
+        bw.flush();
+    }
+
+    static int dfs(int x, int y) {
+        if (dp[x][y] != -1) return dp[x][y];
+
+        dp[x][y] = 1;
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
+            if (map[nx][ny] > map[x][y]) {
+                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny)+1);
+            }
+        }
+
+        return dp[x][y];
+    }
+}
